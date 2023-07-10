a9c7be48747d1d2d8a728f57bd56eee56ff72909
Blurry Face <kakashi.code.take@gmail.com>
2023-07-10 22:48:29 +0530

    Work in progress with Anime Detail

--- src/components/AnimeDetail/AnimeDetail.js
+++ src/components/AnimeDetail/AnimeDetail.js
@@ -2,18 +2,32 @@ import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom'
 import { fetchAsyncAnimeDetail, getAnimeDetail } from '../../redux/slice';
+import './AnimeDetail.scss'
 
 const AnimeDetail = () => {
   const {mal_id} = useParams();
   const dispatch = useDispatch();
   const data = useSelector(getAnimeDetail)
-  console.log(data.title);
+  console.log(data);
 
   useEffect(()=>{
     dispatch(fetchAsyncAnimeDetail(mal_id))
   },[dispatch, mal_id])
   return (
-    <div>AnimeDetail</div>
+    <div className='detail'>
+      <div className='detail-img'>
+        <img src={data.images.jpg.image_url}/>
+      </div>
+      <div className='detail-info'>
+        <div className='detail-title'>
+          {data.title}
+        </div>
+        <div className='detail-title'>
+          {data.studios[0].name}
+        </div>
+      </div>
+
+    </div>
   )
 }
 
